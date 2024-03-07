4a4b94563bb48820fe6acca6451e14367104a0e0
CamilleOnoda <info@camilleonoda.com>
2024-03-07 11:15:16 +0900

    Query elements from the calculator

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const display = document.querySelector('#display');
+    const buttons = document.querySelectorAll('#buttons input[type="button"]');
+    const clearButton = document.querySelector('#clear');
+    const equalButton = document.querySelector('#equal');
+  
+
+});
